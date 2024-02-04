8154693e8ea1caf0cd6852bf13c708e98d52c702
v-lyutin <dyson-sv25@yandex.ru>
2024-02-04 21:10:32 +0300

    feat: create genre-entity, add migration to the genre table

---
+++ src/main/java/com/filmorate/filmorateapi/media/genre/repository/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.filmorate.filmorateapi.media.genre.repository;
+
+import com.filmorate.filmorateapi.media.genre.model.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}
