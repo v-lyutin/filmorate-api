e4bcf003d3d0cd759d966486e363909840089198
v-lyutin <dyson-sv25@yandex.ru>
2024-02-25 22:33:39 +0300

    fix: fix the time output

--- src/main/java/com/filmorate/filmorateapi/media/fact/web/dto/FactResponse.java
+++ src/main/java/com/filmorate/filmorateapi/media/fact/web/dto/FactResponse.java
@@ -1,5 +1,7 @@
 package com.filmorate.filmorateapi.media.fact.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.Instant;
 
 public record FactResponse(
@@ -7,10 +9,12 @@ public record FactResponse(
 
         String text,
 
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
         Instant createdAt,
 
         String createdBy,
 
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "UTC")
         Instant editedAt,
 
         String editedBy) {
